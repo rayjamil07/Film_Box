a036b93b03ac024492afc6baba51e337579220a0
Ezra <ezraraddai166@gmail.com>
2025-01-30 11:36:54 +0300

    MovieContainer

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -1,15 +1,22 @@
-//Movie container
+ 
 import React from 'react';
 import MovieCard from './MovieCard';
 
-function MovieContainer({ movies }) {
-  return (
-    <div className="movie-container">
+function MovieContainer ({movies, onFavorite, onWatched, favorites, watched}){
+  return(
+    <div>
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard
+        key={movie.id}
+        movie={movie}
+        onFavorite={onFavorite}
+        onWatched={onWatched}
+        favorites={favorites}
+        watched={watched}
+        />
       ))}
     </div>
-  );
+  )
 }
 
-export default MovieContainer;
+export default MovieContainer
